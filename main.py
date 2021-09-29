bf7e9102628ef0742d876471914784f4c330c4fa
DolhaRaul <raul.dolha@stud.ubbcluj.ro>
2021-09-29 14:42:41 +0300

    ceva

--- main.py
+++ main.py
@@ -2,32 +2,31 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  
+  print("hello world")
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+  print("hello world")
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+  print("hello world")
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  print("hello world")
   
   
 def main():
-  # interfata de tip consola aici
+  print("hello world")
 
 if __name__ == '__main__':
   main()
